8c48a87e0f39eef26b47dc7a64f3c6954b557c74
Cyan Tabby <69196138+CyanTabby@users.noreply.github.com>
2023-01-02 18:20:31 -1000

    Update VRChatMinus-PS.ps1
    - Fixed dumb regex mistake
    - Removed launch arguments for sdk and api log levels being included by default

--- VRChatMinus-PS.ps1
+++ VRChatMinus-PS.ps1
@@ -1,4 +1,4 @@
-#
+# https://github.com/CyanTabby/VRChatMinus-PS
 
 # VRChat's Steam app ID
 $AppId = '438100'
@@ -11,7 +11,7 @@ $SteamInstallPath = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Wow6432Node\Valve\Stea
 # Saves contents of Steam's libraryfolders.vdf (Valve data file) to a variable as a single string
 $LibraryFolders = Get-Content $SteamInstallPath\steamapps\libraryfolders.vdf -Delimiter 'delimit me daddy~ UwU'
 # RegEx to get the path of the Steam library that VRChat is in (not thoroughly tested yet)
-If (-Not ($LibraryFolders -Match "(?s)path`"\s*`"(?:(?!path).)*path`"\s*`"(?<librarypath>.*?)`".*$AppId")) {
+If (-Not ($LibraryFolders -Match "(?s).*path`"\s*?`"(?<librarypath>.*?)`"(?:(?!path).)*$AppId")) {
     Write-Host "Error: Couldn't find Steam library path in libraryfolders.vdf for $ProcessName (AppId: $AppId)" -ForegroundColor Red
     Write-Host "Closing this window in 30 seconds..." -ForegroundColor DarkGray
     Start-Sleep 30
@@ -25,8 +25,8 @@ $Shortcut = (New-Object -ComObject ("WScript.Shell")).CreateShortcut("$CurrentPa
 $Shortcut.TargetPath = "PowerShell"
 # Here's where you can modify or add your preferred launch arguments for VRChat (list of common options at https://docs.vrchat.com/docs/launch-options)
 #                                                           |           |           |
-#                                                           V           V           V
-$Shortcut.Arguments = " -Command `"& '.\VRChatMinus-PS.ps1' --profile=0 --fps=144 --enable-debug-gui --enable-sdk-log-levels --log-debug-levels=API --osc=9000:127.0.0.1:9001`""
+# --enable-sdk-log-levels --log-debug-levels=API            V           V           V
+$Shortcut.Arguments = " -Command `"& '.\VRChatMinus-PS.ps1' --profile=0 --fps=144 --enable-debug-gui --osc=9000:127.0.0.1:9001`""
 $Shortcut.Description = "Launch VRChat and kill the background EAC process"
 # Uses provided shortcut icon if it exists, otherwise uses the VRChat icon
 If (Test-Path "$CurrentPath\VRChatMinus-PS.ico") {
